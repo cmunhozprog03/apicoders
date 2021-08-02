d314db3894c1acecba090ca1db2d3863be7a061d
cmunhozprog03 <cmunhozprog03@hotmail.com>
2021-08-02 15:03:04 -0300

    config

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Traits\apiTrait;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, apiTrait;
+    use HasApiTokens, HasFactory, Notifiable, apiTrait;
 
     /**
      * The attributes that are mass assignable.
